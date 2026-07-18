162c2bcf051ce37c5b7acda92fa3817d7188fab9
Lucasii2 <kgethomakofane@gmail.com>
2026-07-18 16:00:23 +0200

    Update product.controller.ts

--- demo-one/backend/src/controllers/product.controller.ts
+++ demo-one/backend/src/controllers/product.controller.ts
@@ -29,3 +29,26 @@ export async function getProducts(
 
   return res.json({ products: fallbackProducts });
 }
+export const getProductById = async (req: Request, res: Response) => {
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      return res.status(404).json({
+        error: "Product not found",
+      });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      error: "Server error",
+    });
+  }
+};
