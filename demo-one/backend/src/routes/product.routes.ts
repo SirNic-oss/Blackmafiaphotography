78ce476bc464632f3fd572c07f13e39166459dfa
Lucasii2 <kgethomakofane@gmail.com>
2026-07-18 15:57:31 +0200

    Update product.routes.ts

--- demo-one/backend/src/routes/product.routes.ts
+++ demo-one/backend/src/routes/product.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/product.controller";
+import { getProducts,getProductById } from "../controllers/product.controller";
 
 const router = Router();
 
